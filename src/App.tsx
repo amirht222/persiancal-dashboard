701d05c3481617640911df89fd9c73772bcb13b6
amirhosein <ah.t222amir22@gmail.com>
2024-03-27 21:06:11 +0330

    change App.tsx (create theme + rtl)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { faIR } from "@mui/material/locale";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import React, { useMemo, useState } from "react";
+import { RouterProvider } from "react-router-dom";
+import { prefixer } from "stylis";
+import rtlPlugin from "stylis-plugin-rtl";
+import "./assets/css/Fontiran.css";
+import router from "./routers/MainRouter";
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode] = useState<"light" | "dark">("light");
+
+  const theme = useMemo(
+    () =>
+      createTheme(
+        {
+          direction: "rtl",
+          palette: {
+            primary: {
+              main: "#407BFF",
+              light: "#E9F3FF",
+              dark: "#193E92",
+            },
+            secondary: {
+              main: "#999",
+              light: "#F8FAFC",
+            },
+            success: {
+              main: "#3CD4A0",
+              contrastText: "#fff",
+            },
+            mode,
+          },
+          typography: {
+            fontFamily: "IRANSans",
+            allVariants: {
+              fontWeight: 500,
+            },
+          },
+        },
+        faIR
+      ),
+    [mode]
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default App
+export default App;
