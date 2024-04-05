c7592630254fd5785d4b4de09ea1a8fa4aa9c9db
amirhosein <ah.t222amir22@gmail.com>
2024-04-05 22:49:41 +0330

    add providers around router

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,17 @@ import { prefixer } from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
 import "./assets/css/Fontiran.css";
 import router from "./routers/MainRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "./contexts/SnackbarContext";
+import { ConfirmDialogProvider } from "./contexts/ConfirmDialogContext";
 
 // Create rtl cache
 const cacheRtl = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
 });
+
+const queryClient = new QueryClient();
 
 function App() {
   const [mode] = useState<"light" | "dark">("light");
@@ -45,11 +50,17 @@ function App() {
   );
 
   return (
-    <CacheProvider value={cacheRtl}>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </CacheProvider>
+    <QueryClientProvider client={queryClient}>
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <SnackbarProvider>
+            <ConfirmDialogProvider>
+              <RouterProvider router={router} />
+            </ConfirmDialogProvider>
+          </SnackbarProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </QueryClientProvider>
   );
 }
 
