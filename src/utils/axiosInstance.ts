fb1f302f64839f58bf75939349043e9595f7da06
amirhosein <ah.t222amir22@gmail.com>
2024-04-05 23:01:43 +0330

    implement a reusable axiosInstance with interceptor

---
+++ src/utils/axiosInstance.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3500/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.clear();
+      window.location.pathname = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
