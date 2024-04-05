5346279c970a79721bc7235655bf50c793d678e1
amirhosein <ah.t222amir22@gmail.com>
2024-04-05 22:51:35 +0330

    implement Sidebar component

---
+++ src/components/layout/sidebar/index.tsx
@@ -0,0 +1,100 @@
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import SchoolIcon from "@mui/icons-material/School";
+import ScienceIcon from "@mui/icons-material/Science";
+import {
+  Divider,
+  IconButton,
+  List,
+  Toolbar,
+  styled,
+  useTheme,
+} from "@mui/material";
+import MuiDrawer from "@mui/material/Drawer";
+
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
+
+import MenuItem from "./menu-item";
+
+const KChatDrawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    width: 200,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+const Sidebar = (props: { open: boolean; toggleDrawer: () => void }) => {
+  const theme = useTheme();
+
+  return (
+    <KChatDrawer variant="permanent" open={props.open}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          px: [1],
+        }}
+      >
+        <IconButton onClick={props.toggleDrawer}>
+          {theme.direction === "rtl" ? (
+            <ChevronRightIcon sx={{ color: "white" }} />
+          ) : (
+            <ChevronLeftIcon sx={{ color: "white" }} />
+          )}
+        </IconButton>
+      </Toolbar>
+      <Divider />
+      <List sx={{ py: 0 }}>
+        <MenuItem
+          path={"/users"}
+          title={"کاربران"}
+          icon={<AccountCircleIcon />}
+          isMenuOpen={props.open}
+        />
+        <MenuItem
+          path={"/products"}
+          title={"تجهیزات"}
+          icon={<MedicalServicesIcon />}
+          isMenuOpen={props.open}
+        />
+        <MenuItem
+          path={"/courses"}
+          title={"دوره ها"}
+          icon={<SchoolIcon />}
+          isMenuOpen={props.open}
+        />
+        <MenuItem
+          path={"/labs"}
+          title={"آزمایشگاه ها"}
+          icon={<ScienceIcon />}
+          isMenuOpen={props.open}
+        />
+      </List>
+    </KChatDrawer>
+  );
+};
+
+export default Sidebar;
