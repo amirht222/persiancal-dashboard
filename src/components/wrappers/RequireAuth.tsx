16654915d67620e21d937918f238262375418926
amirhosein <ah.t222amir22@gmail.com>
2024-08-08 22:24:03 +0330

    debug role check

--- src/components/wrappers/RequireAuth.tsx
+++ src/components/wrappers/RequireAuth.tsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const RequireAuth = (props: PropsWithChildren) => {
   if (
     !localStorage.getItem("accessToken") ||
-    localStorage.getItem("role") !== "admin"
+    localStorage.getItem("role") !== "2"
   ) {
     return <Navigate to="/login" replace />;
   }
